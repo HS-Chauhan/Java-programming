0c054a4424f66bf02335c2ad9b1a7cb340cd1bf8
Harshvardhan Singh Chauhan <95760077+HS-Chauhan@users.noreply.github.com>
2023-03-31 10:45:06 +0530

    Update Factorial(Recursion).java

--- Factorial(Recursion).java
+++ Factorial(Recursion).java
@@ -7,7 +7,7 @@
      }    
      public static void main(String args[]){  
       int i,fact=1;  
-      int number=4;//It is the number to calculate factorial    
+      int number=4;   
       fact = factorial(number);   
       System.out.println("Factorial of "+number+" is: "+fact);    
      }  
