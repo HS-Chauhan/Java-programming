38bf16a055ec8ecdbb9133513861e1dfefd8a26c
Harshvardhan Singh Chauhan <95760077+HS-Chauhan@users.noreply.github.com>
2023-04-18 19:10:43 +0530

    Create Trigno-Sin.java

---
+++ Trigno-Sin.java
@@ -0,0 +1,20 @@
+import java.util.*;
+  
+class trignometry{
+  
+    // Driver Code
+    public static void main(String args[])
+    {
+  
+        double degrees = 45.0;
+  
+        
+        double radians = Math.toRadians(degrees);
+  
+        
+        double sinValue = Math.sin(radians);
+  
+        
+        System.out.println("sin(" + degrees + ") = " + sinValue);
+    }
+}
