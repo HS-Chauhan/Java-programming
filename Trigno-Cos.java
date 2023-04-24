033a94940ba2511fbc022b57c452a648c8feed83
Harshvardhan Singh Chauhan <95760077+HS-Chauhan@users.noreply.github.com>
2023-04-24 19:42:03 +0530

    Create Trigno-Cos.java

---
+++ Trigno-Cos.java
@@ -0,0 +1,20 @@
+import java.util.*;
+  
+class GFG {
+  
+   
+    public static void main(String args[])
+    {
+  
+        double degrees = 45.0;
+  
+        
+        double radians = Math.toRadians(degrees);
+  
+       
+        double cosValue = Math.cos(radians);
+  
+        
+        System.out.println("cos(" + degrees + ") = " + cosValue);
+    }
+}
