bf4a57ff4deae9996ce629776fb5b2a06e283123
Harshvardhan Singh Chauhan <95760077+HS-Chauhan@users.noreply.github.com>
2023-05-05 23:47:50 +0530

    Create block with exception.java

---
+++ block with exception.java
@@ -0,0 +1,44 @@
+class finallyBlockWithException{
+
+    static void procA(){
+        try{
+            System.out.println("inside procA");
+            throw new RuntimeException("demo")
+        }
+        finally {
+            System.out.println("procA's finally");
+        }
+    }
+
+    static void procB(){
+        try{
+            System.out.println("inside procB");
+            return;
+        }
+        finally {
+            System.out.println("procB's finally");
+        }
+    }
+
+    static void procC(){
+        try{
+            System.out.println("inside procC");
+            return;
+        }
+        finally {
+            System.out.println("procC's finally");
+        }
+    }
+
+    public static void main(String args[]){
+        try{
+            procA();
+        }
+        catch (Exception e){
+            System.out.println("Exception caught");
+        }
+        procB();
+        procC();
+    }
+
+}
